4ccdc6bafa97add514b47d23fbdd7e175bd77a77
赵志伟 <zhaozhiwei@shinho.net.cn>
2023-03-25 14:27:37 +0800

    main

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 赵志伟 zhaozhiwei@shinho.net.cn
+ * @Date: 2023-03-25 14:20:19
+ * @LastEditors: 赵志伟 zhaozhiwei@shinho.net.cn
+ * @LastEditTime: 2023-03-25 14:22:25
+ * @FilePath: /vue2.0-ui-components/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Vue from 'vue'
 import App from './src/App.vue'
 
@@ -5,3 +13,4 @@ import App from './src/App.vue'
 new Vue({
     render: h => h(App)
 }).$mount("#app")
+console.log(1)
